83fe00e3b09c7bd416b41583a9e01c385e274160
Markus Opolka <markus@martialblog.de>
2020-04-22 15:49:43 +0200

    [workaround] Disable Traefik API Path

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -26,7 +26,7 @@ class vision_traefik::docker (
           'labels'        => [
             'traefik.enable=true',
             'traefik.http.services.traefik.loadbalancer.server.port=8080',
-            'traefik.http.routers.traefik.rule=PathPrefix(`/traefik`) || PathPrefix(`/api`)',
+            'traefik.http.routers.traefik.rule=PathPrefix(`/traefik`)',
             'traefik.http.routers.traefik.service=api@internal',
             'traefik.http.routers.traefik.entrypoints=https',
             'traefik.http.routers.traefik.tls=true',
